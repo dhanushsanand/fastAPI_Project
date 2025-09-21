cdea90b1c1bdea9a21f810e7047db50416167bc2
dhanushsanand <dhanushsanand@gmail.com>
2025-09-20 21:11:22 -0700

    Started Patient Tracker Project

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+  with open('patients.json', 'r') as file:
+    data = json.load(file)
+    return data
+
 @app.get("/")
 def hello():
-  return {'message':'Hello World'}
+  return {'message':'Patient Management System API'}
 
 @app.get("/about")
 def about():
-  return {'message':'Remember the name'}
+  return {'message':'Fully Functional API to manage Patient records'}
+
+
+@app.get("/view")
+def view():
+  return load_data()
