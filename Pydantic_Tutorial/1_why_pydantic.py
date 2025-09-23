3680baca5963e440d25579edc4b8aa6e2c627e27
Dhanush Shivanand <dhanushsanand@gmail.com>
2025-09-22 19:05:23 -0700

    Pydantic Data Validation

--- Pydantic_Tutorial/1_why_pydantic.py
+++ Pydantic_Tutorial/1_why_pydantic.py
@@ -1,16 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
   name:str
+  email:EmailStr
+  linkedin_url: AnyUrl
   age:int
-  weight:float
+  weight:float = Field(gt=0, lt=120)
+  allergies:Optional[List[str]] = Field(max_length=5)
+  contact_details:Dict[str,str]
+  married:Optional[bool] = False
 
 def insert_patient_data(patient:Patient):
   print(patient.name)
   print(patient.age)
+  print(patient.allergies)
   print('Inserted into Database')
 
-patient_info = {'name':'Dhanush', 'age':30}
+
+#'allergies':['Gluten','Pollen']
+patient_info = {'name':'Dhanush', 'age':30, 'weight':76.2,'email':'dhanush@gmail.com', 'contact_details':{'phone_num':'1234567890','email':'abc@gmail.com'}}
 
 patient1 = Patient(**patient_info)
 
@@ -19,6 +28,8 @@ patient1 = Patient(**patient_info)
 def update_patient_data(patient:Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
     print('Updated info')
     
 update_patient_data(patient1)
