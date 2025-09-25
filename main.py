44139e2fc2984ebf9af36ecef838119c3a228578
Dhanush Shivanand <dhanushsanand@gmail.com>
2025-09-25 12:41:31 -0700

    Added Create API endpoint

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
 from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
 import json
+from typing import Annotated, Literal
 
 app = FastAPI()
+
+class Patient(BaseModel):
+  id:Annotated[str, Field(..., description='Id of Patient', examples=['P001'])]
+  name:Annotated[str, Field(..., description='Name of the Patient')]
+  city:Annotated[str, Field(..., description='City of Patient')]
+  age:Annotated[int, Field(..., gt=0, lt=120)]
+  gender:Annotated[Literal['male','female'], Field(..., description='Gender of the patient')]
+  height:Annotated[float, Field(...,gt=0, description='Height of the patient in mtrs')]
+  weight:Annotated[float, Field(..., gt=0, description='Weight of the patient in Kgs')]
+  
+  @computed_field
+  @property
+  def bmi(self)->float:
+    bmi = round(self.weight/(self.height**2),2)
+    return bmi
+  
+  @computed_field
+  @property
+  def verdict(self)->str:
+    if self.bmi < 18.5:
+      return 'Underweight'
+    elif 18.5 <self.bmi < 30:
+      return 'Normal'
+    else:
+      return 'Obese'
+  
+
+
+@app.post('/create')
+def create_patient(patient:Patient):
+  # load existing data
+  data = load_data()
+  
+  #check if patient exists
+  if patient.id in data:
+    raise HTTPException(status_code=400, detail='Patient exists')
+  
+  #new patient added to db
+  data[patient.id] = patient.model_dump(exclude={'id'})
+  save_data(data)
+  
+  return JSONResponse(status_code=201, content={'message':'Patient creation success'})
+  
+    
+
+def save_data(data):
+  with open('patients.json', 'w') as file:
+    json.dump(data, file)
 
 def load_data():
   with open('patients.json', 'r') as file:
